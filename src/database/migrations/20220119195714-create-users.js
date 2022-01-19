a966d3275ed7ca48d309aa52e530be1e6f23cbaf
Adriel.Modollo <adriel.modollo@saudeid.com.br>
2022-01-19 17:02:33 -0300

    Migrations up

---
+++ src/database/migrations/20220119195714-create-users.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTERGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
