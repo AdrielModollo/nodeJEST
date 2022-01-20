9c9538f0357ca1190bed2c4985841d3cc4c32b16
Adriel.Modollo <adriel.modollo@saudeid.com.br>
2022-01-20 10:19:22 -0300

    USER add pg

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
+
+User.create({
+    name: "Adriel",
+    email: "adrielmodollo@outlook.com", 
+    password_hash: "32151567"
+})
 
 module.exports = routes;
