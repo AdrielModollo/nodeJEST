bd7865699a619e3bf94d4dcdacea6582250d27f9
Adriel.Modollo <adriel.modollo@saudeid.com.br>
2022-01-20 13:02:42 -0300

    testes de integração

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+describe('Authentication', () => {
+    it('should sum two number', () => {
+        const x = 2;
+        const y = 4;
+
+        const sum = x + y;
+        
+        expect(sum).toBe(6); //toBe espera que o resultado seja verdadeiro e real.
+    });
+});
+
